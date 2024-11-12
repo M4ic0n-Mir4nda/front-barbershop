f5bd1dacff96bf7eeb93897482119746bc13e1fa
M4ic0n-Mir4nda <maiconmirandasantana@gmail.com>
2024-11-11 22:04:11 -0300

    Second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,38 @@
 import { Box, Typography } from "@mui/material";
-import imgCover from '../images/cover.png';
-import LogoAdm from '../images/logo.png'
-import '../styles/index.css';
+import imgCover from "../images/cover.png";
+import LogoAdm from "../images/logo.png";
+import "../styles/index.css";
 
 function Home() {
-  return(
+  return (
     <>
-      <Box sx={{backgroundImage: `url(${imgCover})`, display: 'flex', alignItems: 'center', justifyContent: 'center', height: '30vh'}}>
-        <img src={LogoAdm} alt="Logo" className="logoAdmin"/>
+      <Box
+        sx={{
+          backgroundImage: `url(${imgCover})`,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "30vh",
+        }}
+      >
+        <img src={LogoAdm} alt="Logo" className="logoAdmin" />
       </Box>
 
-      <Box sx={{backgroundColor: '#FFFAF0', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', height: '70vh'}}>
-        <Typography sx={{fontSize: '35px', marginTop: '50px', marginBottom: '30px'}}>Leve sua barbearia para o próximo nível!</Typography>
+      <Box
+        sx={{
+          backgroundColor: "#FFFAF0",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          height: "70vh",
+        }}
+      >
+        <Typography
+          sx={{ fontSize: "35px", marginTop: "50px", marginBottom: "30px" }}
+        >
+          Leve sua barbearia para o próximo nível!!
+        </Typography>
       </Box>
     </>
   );
