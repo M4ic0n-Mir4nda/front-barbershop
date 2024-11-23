0797489f936f4af78e3e0b668fa2f4a33bec217f
M4ic0n-Mir4nda <maiconmirandasantana@gmail.com>
2024-11-23 01:41:17 -0300

    Update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  BACKEND_URL: "http://34.74.115.158:8080",
-  API_WHATSAPP: "http://34.74.115.158:3010",
+  BACKEND_URL: "http://34.75.63.10:8080",
+  API_WHATSAPP: "http://34.75.63.10:3010",
 };
 
 export default config;
