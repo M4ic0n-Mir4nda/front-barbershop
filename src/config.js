bceaaa3ff990cf33e7a728757a73a48ac589a5d3
M4ic0n-Mir4nda <maiconmirandasantana@gmail.com>
2025-07-25 01:04:14 -0300

    alterando config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 const config = {
-  BACKEND_URL: "http://34.75.63.10:8080",
-  API_WHATSAPP: "http://34.75.63.10:3010",
+  BACKEND_URL: "http://localhost:8080",
+  API_WHATSAPP: "http://localhost:3010",
+  // BACKEND_URL: "http://34.75.63.10:8080",
+  // API_WHATSAPP: "http://34.75.63.10:3010",
 };
 
 export default config;
